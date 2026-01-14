237a20f39226d4663e53cc0c720c6c33fe5a076d
diegoct6 <diegoct6@gmail.com>
2026-01-14 23:19:30 +0100

    attempt 2

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setOptions({
+    path: "panel.html",
+    enabled: true
+  });
+});
+
 chrome.runtime.onMessage.addListener((msg, sender) => {
-  if (msg.type === "OPEN_PANEL") {
+  if (msg.type === "OPEN_PANEL" && sender.tab?.id) {
     chrome.sidePanel.open({ tabId: sender.tab.id });
     chrome.storage.session.set({ lastText: msg.text });
   }
